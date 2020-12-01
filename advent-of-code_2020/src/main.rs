a23cc8f4e41e822739eff37b3154f1d748dc0bdf
Adrian Sadłocha <adrian.adek@gmail.com>
2020-12-01 14:03:20 +0100

    Add day #01 of AoC 2020

--- advent-of-code_2020/src/main.rs
+++ advent-of-code_2020/src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let nums: Vec<i32> = io::stdin().lock().lines()
+        .map(|line| line.unwrap())
+        .map(|line| line.parse().unwrap())
+        .collect();
+    'part1: for n1 in &nums {
+        for n2 in &nums {
+            if n1 + n2 == 2020 {
+                println!("Part 1");
+                println!("{}", n1 * n2);
+                break 'part1;
+            }
+        }
+    }
+
+    'part2: for n1 in &nums {
+        for n2 in &nums {
+            for n3 in &nums {
+                if n1 + n2 + n3 == 2020 {
+                    println!("Part 2");
+                    println!("{}", n1 * n2 * n3);
+                    break 'part2;
+                }
+            }
+        }
+    }
 }
