f816e898cfff92bceee0aaef77d4ff54e1403612
Adrian Sadłocha <adrian@sadlocha.eu>
2021-07-26 22:43:38 +0200

    Add `Makefile`

---
+++ Makefile
@@ -0,0 +1,3 @@
+index.html: src/Main.elm
+	elm make src/Main.elm
+
--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     elmPackages.elm
     elmPackages.elm-format
+    gnumake
   ];
 }
 
