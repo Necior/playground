3b235c1ad9b8e48388d384f008e80f93b55a242a
Adrian Sadłocha <adrian@sadlocha.eu>
2021-07-30 18:23:33 +0200

    Add missing type annotations

--- polls/views.py
+++ polls/views.py
@@ -12,15 +12,15 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id: int) -> HttpResponse:
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id: int) -> HttpResponse:
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 
-def vote(request, question_id: int) -> HttpResponse:
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse("You're voting on question %s." % question_id)
