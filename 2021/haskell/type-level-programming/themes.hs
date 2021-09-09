b3a5915606e4351d9243472c9e917456ae3b2ab0
Adrian Sadłocha <adrian@sadlocha.eu>
2021-09-09 22:50:11 +0200

    Use `RGB` instead of `String`

--- 2021/haskell/type-level-programming/themes.hs
+++ 2021/haskell/type-level-programming/themes.hs
@@ -1,16 +1,25 @@
 module Color where
 
+import Data.Word
+
 data PolybarColorScheme = PolybarColorScheme
-  { focusedWorkspaceText :: String,
-    focusedWorkspaceBackground :: String,
-    visibleWorkspaceText :: String,
-    visibleWorkspaceBackground :: String
+  { focusedWorkspaceText :: RGB,
+    focusedWorkspaceBackground :: RGB,
+    visibleWorkspaceText :: RGB,
+    visibleWorkspaceBackground :: RGB
   }
+
+data RGB = RGB
+  { rgbRed :: Word8,
+    rgbGreen :: Word8,
+    rgbBlue :: Word8
+  }
+  deriving (Eq, Show)
 
 polybarColorScheme =
   PolybarColorScheme
-    { focusedWorkspaceText = "#dda0dd",
-      focusedWorkspaceBackground = "#2a2035",
-      visibleWorkspaceText = "#dda0dd",
-      visibleWorkspaceBackground = "#2a2035"
+    { focusedWorkspaceText = RGB 0xdd 0xa0 0xdd,
+      focusedWorkspaceBackground = RGB 0x2a 0x20 0x35,
+      visibleWorkspaceText = RGB 0xdd 0xa0 0xdd,
+      visibleWorkspaceBackground = RGB 0x2a 0x20 0x35
     }
