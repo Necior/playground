234c5e7dc54adc4506944f53818527d7252581f9
Adrian Sadłocha <adrian@sadlocha.eu>
2021-09-10 00:47:35 +0200

    Support multiple color types

--- 2021/haskell/type-level-programming/themes.hs
+++ 2021/haskell/type-level-programming/themes.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE GADTs #-}
+
 module Color where
 
 import qualified Data.Map.Strict as Map
 import Data.Word
+import Text.Printf
 
 data PolybarColorScheme = PolybarColorScheme
   { focusedWorkspaceText :: RGB,
@@ -10,6 +13,11 @@ data PolybarColorScheme = PolybarColorScheme
     visibleWorkspaceBackground :: RGB
   }
 
+data SomeColor = forall color. IsColor color => SomeColor color
+
+instance Show SomeColor where
+  show = show . toRGB
+
 data RGB = RGB
   { rgbRed :: Word8,
     rgbGreen :: Word8,
@@ -17,18 +25,43 @@ data RGB = RGB
   }
   deriving (Eq, Show)
 
-newtype ThemeInstance = ThemeInstance {getThemeInstance :: Map.Map String RGB}
+newtype ThemeInstance = ThemeInstance {getThemeInstance :: Map.Map String SomeColor}
 
-myTheme =
-  ThemeInstance . Map.fromList $
-    [ ("foreground", RGB 0x3a 0x20 0x35),
-      ("background", RGB 0xdd 0xa0 0xdd)
-    ]
+class IsColor a where
+  toRGB :: a -> RGB
+
+instance IsColor RGB where
+  toRGB = id
+
+instance IsColor SomeColor where
+  toRGB (SomeColor color) = toRGB color
+
+data AliceBlue = AliceBlue
+
+instance IsColor AliceBlue where
+  toRGB = const $ RGB 0xf0 0xf8 0xff
+
+-- many other X11 color could go here
+
+someRGB :: Word8 -> Word8 -> Word8 -> SomeColor
+someRGB r g b = SomeColor $ RGB r g b
+
+t =
+  ThemeInstance $
+    Map.insert
+      "foreground"
+      (someRGB 255 0 0)
+      (Map.singleton "background" (SomeColor AliceBlue))
 
 polybarColorScheme :: ThemeInstance -> Maybe PolybarColorScheme
 polybarColorScheme (ThemeInstance theme) =
   PolybarColorScheme
-    <$> Map.lookup "foreground" theme
-    <*> Map.lookup "background" theme
-    <*> Map.lookup "foreground" theme
-    <*> Map.lookup "background" theme
+    <$> (toRGB <$> Map.lookup "foreground" theme)
+    <*> (toRGB <$> Map.lookup "background" theme)
+    <*> (toRGB <$> Map.lookup "foreground" theme)
+    <*> (toRGB <$> Map.lookup "background" theme)
+
+toHex :: IsColor a => a -> String
+toHex a =
+  let (RGB r g b) = toRGB a
+   in printf "%02x%02x%02x" r g b
