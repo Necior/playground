910ddc115b9deb65db94781d01fedaa0dca14da8
Adrian Sadłocha <adrian@sadlocha.eu>
2022-01-12 02:16:17 +0100

    `rust/serialization`: add enum serialization

--- rust/serialization/src/main.rs
+++ rust/serialization/src/main.rs
@@ -1,18 +1,31 @@
 use serde::Serialize;
+use Handedness::*;
+
+#[derive(Debug, Serialize)]
+enum Handedness {
+    Right,
+    Left,
+    Other(String),
+}
 
 #[derive(Debug, Serialize)]
 struct Person {
     name: String,
     favorite_numbers: Vec<i32>,
+    handedness: Handedness,
 }
 
 fn main() {
-    let person = Person {
-        name: String::from("Necior"),
-        favorite_numbers: [21, 37].into(),
-    };
-    println!("# Debug print");
-    println!("  {:?}", person);
-    println!("# JSON-serialized");
-    println!("  {}", serde_json::to_string(&person).unwrap());
+    let hs: Vec<Handedness> = vec![Right, Left, Other(String::from("both"))];
+    for h in hs {
+        let person = Person {
+            name: String::from("Necior"),
+            favorite_numbers: [21, 37].into(),
+            handedness: h,
+        };
+        println!("# Debug print");
+        println!("  {:?}", person);
+        println!("# JSON-serialized");
+        println!("  {}", serde_json::to_string(&person).unwrap());
+    }
 }
