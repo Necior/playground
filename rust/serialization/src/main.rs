726dfec15fb7251b34e10f5350567e0048f72b3c
Adrian Sadłocha <adrian@sadlocha.eu>
2022-01-12 01:27:01 +0100

    `rust/serialization`: initial commit

--- rust/serialization/src/main.rs
+++ rust/serialization/src/main.rs
@@ -1,3 +1,18 @@
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Person {
+    name: String,
+    favorite_numbers: Vec<i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Person {
+        name: String::from("Necior"),
+        favorite_numbers: [21, 37].into(),
+    };
+    println!("# Debug print");
+    println!("  {:?}", person);
+    println!("# JSON-serialized");
+    println!("  {}", serde_json::to_string(&person).unwrap());
 }
