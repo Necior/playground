d7f6b969194912114a9c85bc8d11080d91234830
Adrian Sadłocha <adrian@sadlocha.eu>
2022-05-12 17:27:41 +0100

    Create `python/hamlet`

---
+++ python/hamlet/utils.py
@@ -0,0 +1,13 @@
+import re
+from typing import List
+
+
+def extract_numbers(raw: str) -> List[List[int]]:
+    lines = raw.strip().split("\n")
+    lines = [l.replace(",", "") for l in lines]
+    regex = re.compile(r"([0-9]+)[^0-9]")
+
+    def get_numbers(line: str) -> List[int]:
+        return [int(n) for n in regex.findall(line)]
+
+    return [get_numbers(l) for l in lines]
